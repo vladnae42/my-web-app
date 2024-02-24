e79e71a3327ccfd9323651fe1b708307fa1bd53c
vlad <vladnae@gmail.com>
2024-02-24 20:05:18 +0200

    edit 3

--- web.py
+++ web.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import functions
 
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
-
 todos = functions.get_todos()
 st.title("Aplicatie web de test")
 st.subheader("Asta este aplicatia web pt To-do. Update.")
 st.write("Aplicatia este facuta in lectia de pyton nr 19 !")
+
+st.text_input(label="text", placeholder='Add new todo...',
+              key='new_todo', on_change=add_todo)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=index)
@@ -20,5 +23,4 @@ for index, todo in enumerate(todos):
         del st.session_state[index]
         st.rerun()
 
-st.text_input(label="text", placeholder='Add new todo...',
-              key='new_todo', on_change=add_todo)
+
